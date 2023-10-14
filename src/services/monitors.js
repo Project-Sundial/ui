e9f1917b6a499717af60dfc38c12d69822a683a6
sofalere <sofalere@gmail.com>
2023-10-14 12:46:12 -0500

    created functions that fetch from API

---
+++ src/services/monitors.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+import {
+  BASE_URL,
+  CREATE_MONITOR,
+  GET_MONITORS
+} from "../constants/routes";
+
+export const getMonitors = async () => {
+  const { data } = await axios.get(BASE_URL + GET_MONITORS);
+  return data;
+};
+
+export const createMonitor = async (newMonitor) => {
+  const { data } = await axios.post(BASE_URL + CREATE_MONITOR, { ...newMonitor });
+  return data;
+};
