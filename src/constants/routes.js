fa4a8c153de8a1e7911be743ab0073e8fc6881f6
Jacob Clark <jacobclark1198@gmail.com>
2023-10-18 15:51:32 +0100

    use react_app env variables

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,3 +1,3 @@
-export const BASE_URL = 'http://localhost:3010'; // should be env variable
+export const BASE_URL = process.env.REACT_APP_SERVER_URL;
 export const GET_MONITORS = '/api/monitors';
 export const CREATE_MONITOR = '/api/monitors';
