32d179ba8ab97e79cae6905003c16b038b57398a
Jacob Clark <102357760+Jacob-Clark-809@users.noreply.github.com>
2023-10-18 19:54:32 +0100

    Update generateCurl.js
    Added -X POST to curl command

--- src/utils/generateCurl.js
+++ src/utils/generateCurl.js
@@ -5,7 +5,7 @@ const generateCurl = (monitor) => {
     monitor.command : '<COMMAND>';
 
   return `${monitor.schedule} ${command} ` +
-    `&& curl ${BASE_URL + CREATE_PING + '/' + monitor.endpoint_key}`;
+    `&& curl -X POST ${BASE_URL + CREATE_PING + '/' + monitor.endpoint_key}`;
 };
 
 export default generateCurl;
