9677b4c22d1747c11fd464ff053f4441d38d3cb2
Jacob Clark <jacobclark1198@gmail.com>
2023-10-18 16:15:22 +0100

    generateCurl now takes the whole monitor as a parameter and generates a more precise wrapper string

--- src/components/Monitor.jsx
+++ src/components/Monitor.jsx
@@ -11,7 +11,7 @@ export const Monitor = ({ monitor, count }) => {
             secondary={
               <div>
                 <Typography variant="subtitle1">Name: {monitor.name}</Typography>
-                <Typography variant="subtitle1">Wrapper: {generateCurl(monitor.endpoint_key)}</Typography>
+                <Typography variant="subtitle1">Wrapper: {generateCurl(monitor)}</Typography>
                 <Typography variant="subtitle1">Schedule: {monitor.schedule}</Typography>
                 <Typography variant="subtitle1">Next Expected At: {monitor.next_expected_at}</Typography>
                 <Typography variant="subtitle1">Failed: {monitor.failed}</Typography>
--- src/utils/generateCurl.js
+++ src/utils/generateCurl.js
@@ -1,6 +1,11 @@
-const generateCurl = (endpoint_key) => {
-  return `<COMMAND> ` +
-    `&& curl <SERVER URL>/pings/${endpoint_key}`;
+import { BASE_URL, CREATE_PING } from "../constants/routes";
+
+const generateCurl = (monitor) => {
+  const command = monitor.command ? 
+    monitor.command : '<COMMAND>';
+
+  return `${monitor.schedule} ${command} ` +
+    `&& curl ${BASE_URL + CREATE_PING + '/' + monitor.endpoint_key}`;
 };
 
 export default generateCurl;
