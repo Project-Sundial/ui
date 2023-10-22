db4e6bf48ca9dd891cbddfec3fde64442f8ea3e6
sofalere <sofalere@gmail.com>
2023-10-22 11:59:36 -0500

    docker files

--- src/constants/routes.js
+++ src/constants/routes.js
@@ -1,4 +1,5 @@
-export const BASE_URL = process.env.REACT_APP_SERVER_URL;
+// export const BASE_URL = process.env.REACT_APP_SERVER_URL;
+export const BASE_URL = 'http://localhost:58669';
 export const GET_MONITORS = '/api/monitors';
 export const CREATE_MONITOR = '/api/monitors';
 export const CREATE_PING = '/api/pings';
